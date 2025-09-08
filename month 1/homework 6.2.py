ce6e5ad4d83bb78d59801f6436111abf94b0c79c
liliya-fyg <liliya.1605.07@gmail.com>
2025-09-08 16:41:39 +0600

    Delete month 1 directory

--- month 1/homework 6.2.py
+++
@@ -1,7 +0,0 @@
-def nearest_number(numbers, target=0):
-    return min(numbers, key=lambda x: abs(x - target))
-
-print(nearest_number([1, 2, 3], 5))
-print(nearest_number([-10, -5, 0, 2], 1))
-print(nearest_number([4.5, 6.7, 9.1], 7))
-
