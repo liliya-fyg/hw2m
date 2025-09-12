31e8a8f5479f4ad7fd56a6be40957dcd7155791b
liliya-fyg <liliya.1605.07@gmail.com>
2025-09-12 22:25:47 +0600

    hw3.py added

---
+++ main.py
@@ -0,0 +1,6 @@
+# from my_pachege.module_1 import add, test
+#
+# print(add(23, 32))
+# print(test)
+
+print("Some text")
---
+++ my_pachege/module_1.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+test = "123"
+
+# __all__ = ()
+
