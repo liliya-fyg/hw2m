867debcece27c0c8e3f9b76e0e3b8b19b3749776
liliya-fyg <liliya.1605.07@gmail.com>
2025-09-17 11:52:08 +0600

    Добавлены main.py и requirements.txt

--- main.py
+++ main.py
@@ -3,4 +3,14 @@
 # print(add(23, 32))
 # print(test)
 
-print("Some text")
+# print("Some text")
+
+import requests
+
+response = requests.get("https://api.github.com")
+
+if response.status_code == 200:
+    print("Успешный запрос!")
+    print(response.json())
+else:
+    print("Ошибка:", response.status_code)
